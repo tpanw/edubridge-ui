457f6b9fcd2edeea44eb0891b40276476935de25
tpanw <tpanw@allstate.com>
2020-05-25 01:58:27 +0530

    Updates

---
+++ precache-manifest.8d91ffcf414188c1fbda69e59afaa270.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4dd8856edf9db658a95737aae35af1d",
+    "url": "/edubridge-ui/index.html"
+  },
+  {
+    "revision": "6a7b6efde00e78db54fc",
+    "url": "/edubridge-ui/static/css/main.5365bfa4.chunk.css"
+  },
+  {
+    "revision": "da58d85efd87539b65ad",
+    "url": "/edubridge-ui/static/js/2.4c52b96a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/edubridge-ui/static/js/2.4c52b96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7b6efde00e78db54fc",
+    "url": "/edubridge-ui/static/js/main.3915248d.chunk.js"
+  },
+  {
+    "revision": "18ca90b4c7c5fa80da04",
+    "url": "/edubridge-ui/static/js/runtime-main.4dc2a817.js"
+  },
+  {
+    "revision": "7e6361a1b7ee68fd843c470516178ff4",
+    "url": "/edubridge-ui/static/media/background.7e6361a1.jpg"
+  },
+  {
+    "revision": "cc82aacdd668eedc1fee624a98574bec",
+    "url": "/edubridge-ui/static/media/group.cc82aacd.svg"
+  },
+  {
+    "revision": "b0d0d32ef0f8ecacd7c2f263d8026f2a",
+    "url": "/edubridge-ui/static/media/individual.b0d0d32e.svg"
+  },
+  {
+    "revision": "b4db83e2c89826c8e486b42604eecc1f",
+    "url": "/edubridge-ui/static/media/logo.b4db83e2.svg"
+  },
+  {
+    "revision": "ae9bae263ed27be66a775eff13e603c5",
+    "url": "/edubridge-ui/static/media/teacher.ae9bae26.jpg"
+  }
+]);
